c2c99525fe8b567fa00757cf5f6ec7222a5309a4
Adriano Trancoso <verythrax@gmail.com>
2021-05-06 19:21:47 -0300

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float RotationSpeed;
+	CharacterController cc;
+	private Animator anim;
+	protected Vector3 gravidade = Vector3.zero;
+	protected Vector3 move = Vector3.zero;
+	private bool jump = false;
+
+
+	
+	void Start()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Parado");
+	}
+
+	void Update()
+	{
+		Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
+		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+		
+		if (!cc.isGrounded) {
+			gravidade += Physics.gravity * Time.deltaTime;
+		} 
+		else 
+		{
+			gravidade = Vector3.zero;
+			if(jump)
+			{
+				gravidade.y = 6f;
+				jump = false;
+			}
+		}
+		move += gravidade;
+		cc.Move (move* Time.deltaTime);
+		Anima ();
+	}
+	 
+	void Anima()
+	{
+		if (!Input.anyKey && Input.GetAxis("Vertical") == 0)
+		{
+			anim.SetTrigger("Parado");
+		} 
+		else 
+		{
+			if(Input.GetKeyDown("space"))
+			{
+				anim.SetTrigger("Pula");
+				jump = true;
+			}
+			else
+			{
+				anim.SetTrigger("Corre");
+			}
+		}
+	}
+}
